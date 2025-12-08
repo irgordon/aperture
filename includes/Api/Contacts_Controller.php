4967525105edcdca7674432eea1e424fb3a12c12
Ian <118707480+irgordon@users.noreply.github.com>
2025-12-08 15:47:01 -0500

    Update Contacts_Controller.php

--- includes/Api/Contacts_Controller.php
+++ includes/Api/Contacts_Controller.php
@@ -1,41 +1,26 @@
 <?php
 namespace AperturePro\Api;
-
-use WP_REST_Controller;
-use WP_REST_Server;
-use WP_REST_Response;
+use WP_REST_Controller, WP_REST_Server, WP_REST_Response;
 
 class Contacts_Controller extends WP_REST_Controller {
-
     protected $namespace = 'aperture/v1';
     protected $rest_base = 'contacts';
 
     public function register_routes() {
-        // List Contacts
         register_rest_route( $this->namespace, '/' . $this->rest_base, [
-            'methods'             => WP_REST_Server::READABLE,
-            'callback'            => [ $this, 'get_items' ],
-            'permission_callback' => function() { return current_user_can('manage_options'); },
-        ] );
-
-        // Create/Update Contact
-        register_rest_route( $this->namespace, '/' . $this->rest_base, [
-            'methods'             => WP_REST_Server::CREATABLE,
-            'callback'            => [ $this, 'create_item' ],
-            'permission_callback' => function() { return current_user_can('manage_options'); },
-        ] );
+            ['methods' => WP_REST_Server::READABLE, 'callback' => [ $this, 'get_items' ], 'permission_callback' => function(){return current_user_can('manage_options');}],
+            ['methods' => WP_REST_Server::CREATABLE, 'callback' => [ $this, 'create_item' ], 'permission_callback' => function(){return current_user_can('manage_options');}]
+        ]);
     }
 
     public function get_items( $request ) {
         global $wpdb;
-        $results = $wpdb->get_results( "SELECT * FROM {$wpdb->prefix}ap_contacts ORDER BY created_at DESC" );
-        return rest_ensure_response( $results );
+        return rest_ensure_response( $wpdb->get_results( "SELECT * FROM {$wpdb->prefix}ap_contacts ORDER BY created_at DESC" ) );
     }
 
     public function create_item( $request ) {
         global $wpdb;
         $params = $request->get_json_params();
-        
         $data = [
             'first_name' => sanitize_text_field( $params['first_name'] ),
             'last_name'  => sanitize_text_field( $params['last_name'] ),
@@ -44,13 +29,11 @@ class Contacts_Controller extends WP_REST_Controller {
             'address'    => sanitize_textarea_field( $params['address'] ),
         ];
 
-        // Update if ID exists, otherwise Insert
         if ( ! empty( $params['id'] ) ) {
             $wpdb->update( "{$wpdb->prefix}ap_contacts", $data, [ 'id' => $params['id'] ] );
-            return new WP_REST_Response( [ 'message' => 'Contact updated' ], 200 );
         } else {
             $wpdb->insert( "{$wpdb->prefix}ap_contacts", $data );
-            return new WP_REST_Response( [ 'message' => 'Contact added', 'id' => $wpdb->insert_id ], 201 );
         }
+        return new WP_REST_Response( [ 'message' => 'Saved' ], 200 );
     }
 }
