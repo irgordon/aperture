f2e4063c56ef50920c9d7a45eb6c74e59ba38305
Ian <118707480+irgordon@users.noreply.github.com>
2025-12-07 22:42:14 -0500

    Update index.js

--- src/admin/index.js
+++ src/admin/index.js
@@ -3,6 +3,7 @@ import { HashRouter, Routes, Route, Link } from 'react-router-dom';
 import LeadList from './components/LeadList';
 import SettingsPage from './components/SettingsPage';
 import QuestionnaireBuilder from './components/QuestionnaireBuilder';
+import GalleryManager from './components/GalleryManager'; // Import the new manager
 import './style.scss';
 
 const AdminApp = () => (
@@ -11,12 +12,14 @@ const AdminApp = () => (
             <nav className="ap-nav">
                 <h1>AperturePro</h1>
                 <Link to="/">Pipeline</Link>
+                <Link to="/galleries">Galleries</Link> {/* NEW TAB */}
                 <Link to="/forms">Forms</Link>
                 <Link to="/settings">Settings</Link>
             </nav>
             <main>
                 <Routes>
                     <Route path="/" element={<LeadList />} />
+                    <Route path="/galleries" element={<GalleryManager />} /> {/* NEW ROUTE */}
                     <Route path="/forms" element={<QuestionnaireBuilder />} />
                     <Route path="/settings" element={<SettingsPage />} />
                 </Routes>
